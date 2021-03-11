8b4d443e026afe4bd5beb89f5fbd24516b6f52ca
Ben Sooraj <bensooraj@users.noreply.github.com>
2021-03-11 09:14:23 +0530

    ReceiveMessage

--- hwebsocket/main.go
+++ hwebsocket/main.go
@@ -137,17 +137,19 @@ func (wsc *WebsocketConnection) Connect() error {
 }
 
 // Close ...
-func (wsc *WebsocketConnection) Close() {
+func (wsc *WebsocketConnection) Close() error {
 	close(wsc.CloseChannel)
 
 	err := wsc.Conn.Close()
 	if err != nil {
 		log.Printf("[ws][%s] Error closing the websocket connection: %s", wsc.WebsocketURL, err.Error())
+		return err
 	}
 
 	if wsc.IsDump {
 		log.Printf("[ws][%s] connection closed", wsc.WebsocketURL)
 	}
+	return nil
 }
 
 // WriteRequest ...
@@ -172,6 +174,76 @@ func (wsc *WebsocketConnection) WriteRequest() {
 		if err != nil {
 			log.Printf("[ws][%s] Error writing message: %s", wsc.WebsocketURL, err.Error())
 			time.Sleep(1 * time.Second)
+		}
+	}
+}
+
+// ReceiveMessage ...
+func (wsc *WebsocketConnection) ReceiveMessage(msg []byte) {
+	// CLOSE
+	wsc.Conn.SetCloseHandler(func(code int, text string) error {
+		log.Printf("[ws][%s] websocket exiting [code=%d, text=%s]", wsc.WebsocketURL, code, text)
+		err := wsc.Close()
+		return err
+	})
+
+	// PONG
+	wsc.Conn.SetPongHandler(func(appData string) error {
+		log.Printf("[ws][%s] PONG RECEIVE %s", wsc.WebsocketURL, appData)
+		wsc.Conn.SetReadDeadline(time.Now().Add(wsc.ReadDeadlineTime))
+		return nil
+	})
+
+	// PING
+	wsc.Conn.SetPingHandler(func(appData string) error {
+		wsc.Conn.SetReadDeadline(time.Now().Add(wsc.ReadDeadlineTime))
+		err := wsc.Conn.WriteMessage(websocket.PongMessage, nil)
+		if err != nil {
+			log.Printf("[ws][%s] PING RECEIVE ERROR %s", wsc.WebsocketURL, err.Error())
+			return err
+		}
+		log.Printf("[ws][%s] PING RECEIVED %s", wsc.WebsocketURL, appData)
+		return nil
+	})
+
+	for {
+		select {
+		case <-wsc.CloseChannel:
+			log.Printf("[ws][%s] Exiting the ReceiveMessage goroutine", wsc.WebsocketURL)
+			return
+		default:
+			msgType, msg, err := wsc.Conn.ReadMessage()
+			if err != nil {
+				log.Printf("[ws][%s] Error receiving message from the websocket: %s", wsc.WebsocketURL, err.Error())
+
+				if wsc.IsAutoReconnect {
+					log.Printf("[ws][%s] Attempting reconnect", wsc.WebsocketURL)
+					wsc.Reconnect()
+					continue
+				}
+				// Should this come before the reconnect attempt
+				if wsc.ErrorHandleFunc != nil {
+					wsc.ErrorHandleFunc(err)
+				}
+
+				return
+			}
+
+			wsc.Conn.SetReadDeadline(time.Now().Add(wsc.ReadDeadlineTime * time.Second))
+
+			switch msgType {
+			case websocket.BinaryMessage:
+			case websocket.TextMessage:
+				err = wsc.MessageHandleFunc(msg)
+				if err != nil {
+					log.Printf("[ws][%s] Error processing the message: %s", wsc.WebsocketURL, err.Error())
+				}
+			case websocket.CloseMessage:
+				wsc.Close()
+			default:
+				log.Printf("[ws][%s] unhandled message type %d. Message: %s", wsc.WebsocketURL, msgType, msg)
+			}
+
 		}
 	}
 }
