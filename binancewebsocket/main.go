8e512dc8a016313c52a0f81b07c866c1a4afd93f
Ben Sooraj <bensooraj@users.noreply.github.com>
2021-03-11 13:40:01 +0530

    open binance websocket connection

--- binancewebsocket/main.go
+++ binancewebsocket/main.go
@@ -1,7 +1,49 @@
 package binancewebsocket
 
-// BinanceWebSocket ...
-type BinanceWebSocket struct {
+import (
+	"time"
+
+	"gitlab.com/hooklabs-backend/order-management-system-engine/h-lob-service/hwebsocket"
+)
+
+// BinanceWebsocket ...
+type BinanceWebsocket struct {
 	BaseURL string
-	// Conn    *ws.WsConn
+	Conn    *hwebsocket.WebsocketConnection
+}
+
+// NewBinanceWebsocket ...
+func NewBinanceWebsocket() *BinanceWebsocket {
+	binanceWebsocket := &BinanceWebsocket{}
+
+	return binanceWebsocket
+}
+
+// Open ...
+func (bws *BinanceWebsocket) Open(url string, messageHandleFunc func([]byte) error, errorHandleFunc func(error)) {
+	bws.BaseURL = url
+
+	bws.Conn = hwebsocket.
+		New().
+		SetWebsocketURL(url).
+		SetMessageHandleFunc(messageHandleFunc).
+		SetErrorHandleFunc(errorHandleFunc).
+		SetAutoReconnect(true).
+		SetConnectionRetryLimit(10).
+		Build()
+
+	go func() {
+		ticker := time.NewTicker(30 * time.Second)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case <-bws.Conn.CloseChannel:
+				return
+			case tick := <-ticker.C:
+				bws.Conn.SendPingMessage([]byte(tick.String()))
+			}
+		}
+	}()
+	return
 }
