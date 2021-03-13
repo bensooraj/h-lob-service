99df41b24d2d13d739a2f2cc950e91b94498ccd3
Ben Sooraj <bensooraj@users.noreply.github.com>
2021-03-14 02:25:53 +0530

    major changes LoB with float64 data type

--- limitorderbook/l2lob.go
+++ limitorderbook/l2lob.go
@@ -8,57 +8,72 @@ import (
 type L2LimitOrderBook struct {
 	Bids                   *btree.BTree
 	Asks                   *btree.BTree
-	PricePrecision         float64
-	CumulativeBidLimitsMap map[int64]float64
-	CumulativeAskLimitsMap map[int64]float64
+	PriceScale             float64
+	CumulativeBidLimitsMap map[LoBFloat64]float64
+	CumulativeAskLimitsMap map[LoBFloat64]float64
+}
+
+// LoBInt64 implements the Item interface for int64.
+type LoBInt64 int64
+
+// Less returns true if int64(a) < int64(b).
+func (a LoBInt64) Less(b btree.Item) bool {
+	return a < b.(LoBInt64)
+}
+// LoBInt64 implements the Item interface for int64.
+type LoBFloat64 float64
+
+// Less returns true if int64(a) < int64(b).
+func (a LoBFloat64) Less(b btree.Item) bool {
+	return a < b.(LoBFloat64)
 }
 
 func NewL2LimitOrderBook(pricePrecision float64) *L2LimitOrderBook {
 	return &L2LimitOrderBook{
 		Bids:                   btree.New(2),
 		Asks:                   btree.New(2),
-		PricePrecision:         pricePrecision,
-		CumulativeBidLimitsMap: make(map[int64]float64),
-		CumulativeAskLimitsMap: make(map[int64]float64),
+		PriceScale:             pricePrecision,
+		CumulativeBidLimitsMap: make(map[LoBFloat64]float64),
+		CumulativeAskLimitsMap: make(map[LoBFloat64]float64),
 	}
 }
 
-func (l2lob *L2LimitOrderBook) UpdateOrAdd(price, quantity float64, side string) {
-	adjustedPrice := int64(price * l2lob.PricePrecision)
+func (l2lob *L2LimitOrderBook) UpdateOrAdd(price LoBFloat64, quantity float64, side string) {
+	// adjustedPrice := LoBInt64(price * math.Pow(10, l2lob.PriceScale))
 
 	if side == "a" {
 
-		if _, ok := l2lob.CumulativeAskLimitsMap[adjustedPrice]; !ok {
-			l2lob.Asks.ReplaceOrInsert(btree.Int(adjustedPrice))
+		if _, ok := l2lob.CumulativeAskLimitsMap[price]; !ok {
+			l2lob.Asks.ReplaceOrInsert(price)
 		}
-		l2lob.CumulativeAskLimitsMap[adjustedPrice] = quantity
+		l2lob.CumulativeAskLimitsMap[price] = quantity
 
 	} else if side == "b" {
 
-		if _, ok := l2lob.CumulativeBidLimitsMap[adjustedPrice]; !ok {
-			l2lob.Bids.ReplaceOrInsert(btree.Int(adjustedPrice))
+		if _, ok := l2lob.CumulativeBidLimitsMap[price]; !ok {
+			l2lob.Bids.ReplaceOrInsert(price)
 		}
-		l2lob.CumulativeBidLimitsMap[adjustedPrice] = quantity
+		l2lob.CumulativeBidLimitsMap[price] = quantity
 
 	}
 }
 
-func (l2lob *L2LimitOrderBook) Remove(price float64, side string) {
-	adjustedPrice := int64(price * l2lob.PricePrecision)
+func (l2lob *L2LimitOrderBook) Remove(price LoBFloat64, side string) {
+	// adjustedPrice := LoBInt64(price * math.Pow(10, l2lob.PriceScale))
 
 	if side == "a" {
 
-		if _, ok := l2lob.CumulativeAskLimitsMap[adjustedPrice]; ok {
-			l2lob.Asks.Delete(btree.Int(adjustedPrice))
+		if _, ok := l2lob.CumulativeAskLimitsMap[price]; ok {
+			l2lob.Asks.Delete(price)
 		}
-		delete(l2lob.CumulativeAskLimitsMap, adjustedPrice)
+		delete(l2lob.CumulativeAskLimitsMap, price)
 
 	} else if side == "b" {
 
-		if _, ok := l2lob.CumulativeBidLimitsMap[adjustedPrice]; ok {
-			l2lob.Bids.Delete(btree.Int(adjustedPrice))
+		if _, ok := l2lob.CumulativeBidLimitsMap[price]; ok {
+			l2lob.Bids.Delete(price)
 		}
-		delete(l2lob.CumulativeBidLimitsMap, adjustedPrice)
+		delete(l2lob.CumulativeBidLimitsMap, price)
 
 	}
 }
